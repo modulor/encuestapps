4d33f331e1de284ca919d9b4a73fc58f39ff3ab6
modulor <modulor@hotmail.es>
2016-03-09 17:29:03 -0600

    base de datos...

--- encuestapps.sql
+++ encuestapps.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-03-2016 a las 23:25:05
+-- Tiempo de generación: 10-03-2016 a las 00:28:40
 -- Versión del servidor: 10.1.9-MariaDB
 -- Versión de PHP: 5.6.15
 
@@ -19,7 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `encuestapps`
 --
-/*DROP DATABASE `encuestapps`;*/
 CREATE DATABASE IF NOT EXISTS `encuestapps` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `encuestapps`;
 
@@ -36,14 +35,6 @@ CREATE TABLE `campaigns` (
   `fecha_hora_creacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `datos_clientes_k` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `campaigns`
---
-
-INSERT INTO `campaigns` (`campaigns_k`, `campaign`, `fecha_hora_creacion`, `datos_clientes_k`) VALUES
-(1, 'Campaña Elecciones 2016', '2016-03-09 18:47:50', 1),
-(2, 'Campaña de prueba', '2016-03-09 20:40:16', 1);
 
 -- --------------------------------------------------------
 
@@ -141,16 +132,6 @@ CREATE TABLE `encuestas` (
   `datos_clientes_k` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Volcado de datos para la tabla `encuestas`
---
-
-INSERT INTO `encuestas` (`encuestas_k`, `nombre_encuesta`, `fecha_hora_creacion`, `campaigns_k`, `datos_clientes_k`) VALUES
-(1, 'Elecciones 2016', '2016-03-03 21:26:39', 1, 1),
-(2, 'Encuesta de prueba', '2016-03-03 22:11:05', 1, 1),
-(7, 'Encuesta prueba', '2016-03-09 20:45:35', 2, 1),
-(8, 'Encuesta prueba 2', '2016-03-09 20:47:35', 2, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -163,26 +144,6 @@ CREATE TABLE `encuestas_preguntas` (
   `pregunta` text NOT NULL,
   `encuestas_k` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `encuestas_preguntas`
---
-
-INSERT INTO `encuestas_preguntas` (`encuestas_preguntas_k`, `pregunta`, `encuestas_k`) VALUES
-(1, '¿Si hoy fueran las elecciones por quien votaría?', 1),
-(2, '¿Cuando voto lo hago pensando mas en el partido que en el candidato?', 1),
-(3, 'Creo que el sistema electoral es transparente y honesto', 1),
-(4, 'Creo que todos lo partidos son lo mismo', 1),
-(5, 'En general, el país esta mejor hoy que hace 5 años', 1),
-(6, 'En general mi municipio está mejor hoy que hace 3 años', 1),
-(7, 'En general, pienso que en 5 años estaremos mejor de lo que estamos hoy', 1),
-(8, 'En general, como evaluaría el trabajo del actual mandatario estatal', 1),
-(9, 'Creo que mi voto puede realmente hacer una diferencia', 1),
-(10, 'Tengo una idea de por quien votar, pero cambiaría mi voto si me dieran los argumentos y encontrara razones validas para hacerlo', 1),
-(11, 'Pregunta uno', 2),
-(12, 'Pregunta dos', 2),
-(21, 'pregunta', 7),
-(22, 'pregunta...', 8);
 
 -- --------------------------------------------------------
 
@@ -197,76 +158,6 @@ CREATE TABLE `encuestas_preguntas_opciones` (
   `encuestas_preguntas_k` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Volcado de datos para la tabla `encuestas_preguntas_opciones`
---
-
-INSERT INTO `encuestas_preguntas_opciones` (`encuestas_preguntas_opciones_k`, `opcion`, `encuestas_preguntas_k`) VALUES
-(1, 'PRI', 1),
-(2, 'PAN', 1),
-(3, 'PRD', 1),
-(4, 'Independiente', 1),
-(5, 'Siempre', 2),
-(6, 'Algunas Veces', 2),
-(7, 'Pocas veces', 2),
-(8, 'Nunca', 2),
-(9, 'De acuerdo', 3),
-(10, 'Parcialmente de acuerdo', 3),
-(11, 'Un poco en desacuerdo', 3),
-(12, 'Totalmente en  desacuerdo', 3),
-(13, 'De acuerdo', 4),
-(14, 'Parcialmente de acuerdo', 4),
-(15, 'Un poco en desacuerdo', 4),
-(16, 'Totalmente en  desacuerdo', 4),
-(17, 'Totalmente de acuerdo', 5),
-(18, 'Parcialmente de acuerdo', 5),
-(19, 'Un poco en desacuerdo', 5),
-(20, 'Totalmente en  desacuerdo', 5),
-(21, 'Totalmente de acuerdo', 6),
-(22, 'Parcialmente de acuerdo', 6),
-(23, 'Un poco en desacuerdo', 6),
-(24, 'Totalmente en  desacuerdo', 6),
-(25, 'Por supuesto que sí', 7),
-(26, 'Tal vez', 7),
-(27, 'Probablemente no', 7),
-(28, 'Estaremos peor', 7),
-(29, 'Muy bueno', 8),
-(30, 'Bueno', 8),
-(31, 'Regular', 8),
-(32, 'Malo', 8),
-(33, 'Por supuesto que sí', 9),
-(34, 'Quizá', 9),
-(35, 'Probablemente no', 9),
-(36, 'Claro que no', 9),
-(37, 'No encontraré argumentos ni razones para cambiar', 10),
-(38, 'Sí, pero será difícil validarlos', 10),
-(39, 'Seguramente lo haría', 10),
-(40, 'Claro, siempre estoy analizando la mejor opción', 10),
-(41, 'respuesta a', 11),
-(42, 'respuesta b', 11),
-(43, 'respuesta c', 11),
-(44, 'respuesta d', 11),
-(45, 'respuesta dos a', 12),
-(46, 'respuesta dos b', 12),
-(47, 'respuesta dos c', 12),
-(48, 'respuesta dos d', 12),
-(49, 'a', 13),
-(50, 'b', 13),
-(51, 'c', 13),
-(52, 'd', 13),
-(53, 'a', 14),
-(54, 'b', 14),
-(55, 'c', 14),
-(56, 'd', 14),
-(81, 'respuesta 1', 21),
-(82, 'respuesta 2', 21),
-(83, 'respuesta 3', 21),
-(84, 'respuesta 4', 21),
-(85, 'respuesta 1...', 22),
-(86, 'respuesta 2...', 22),
-(87, 'respuesta 3...', 22),
-(88, 'respuesta 4...', 22);
-
 -- --------------------------------------------------------
 
 --
@@ -280,120 +171,6 @@ CREATE TABLE `encuestas_resultados` (
   `datos_encuestados_k` int(11) NOT NULL,
   `fecha_hora_creacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `encuestas_resultados`
---
-
-INSERT INTO `encuestas_resultados` (`encuestas_resultados_k`, `encuestas_preguntas_opciones_k`, `datos_encuestados_k`, `fecha_hora_creacion`) VALUES
-(1, 1, 0, '2016-03-07 18:11:37'),
-(2, 5, 0, '2016-03-07 18:11:37'),
-(3, 9, 0, '2016-03-07 18:11:37'),
-(4, 13, 0, '2016-03-07 18:11:37'),
-(5, 17, 0, '2016-03-07 18:11:37'),
-(6, 21, 0, '2016-03-07 18:11:38'),
-(7, 25, 0, '2016-03-07 18:11:38'),
-(8, 29, 0, '2016-03-07 18:11:38'),
-(9, 33, 0, '2016-03-07 18:11:38'),
-(10, 37, 0, '2016-03-07 18:11:38'),
-(11, 4, 0, '2016-03-07 18:18:37'),
-(12, 8, 0, '2016-03-07 18:18:37'),
-(13, 12, 0, '2016-03-07 18:18:37'),
-(14, 16, 0, '2016-03-07 18:18:38'),
-(15, 20, 0, '2016-03-07 18:18:38'),
-(16, 24, 0, '2016-03-07 18:18:38'),
-(17, 28, 0, '2016-03-07 18:18:38'),
-(18, 32, 0, '2016-03-07 18:18:38'),
-(19, 36, 0, '2016-03-07 18:18:38'),
-(20, 40, 0, '2016-03-07 18:18:38'),
-(21, 4, 0, '2016-03-07 18:18:43'),
-(22, 8, 0, '2016-03-07 18:18:44'),
-(23, 12, 0, '2016-03-07 18:18:44'),
-(24, 16, 0, '2016-03-07 18:18:44'),
-(25, 20, 0, '2016-03-07 18:18:44'),
-(26, 24, 0, '2016-03-07 18:18:44'),
-(27, 28, 0, '2016-03-07 18:18:44'),
-(28, 32, 0, '2016-03-07 18:18:44'),
-(29, 36, 0, '2016-03-07 18:18:44'),
-(30, 40, 0, '2016-03-07 18:18:44'),
-(31, 2, 0, '2016-03-07 22:01:54'),
-(32, 7, 0, '2016-03-07 22:01:54'),
-(33, 10, 0, '2016-03-07 22:01:54'),
-(34, 15, 0, '2016-03-07 22:01:54'),
-(35, 18, 0, '2016-03-07 22:01:54'),
-(36, 23, 0, '2016-03-07 22:01:54'),
-(37, 26, 0, '2016-03-07 22:01:54'),
-(38, 31, 0, '2016-03-07 22:01:54'),
-(39, 34, 0, '2016-03-07 22:01:54'),
-(40, 39, 0, '2016-03-07 22:01:54'),
-(41, 2, 0, '2016-03-07 22:49:02'),
-(42, 7, 0, '2016-03-07 22:49:02'),
-(43, 10, 0, '2016-03-07 22:49:02'),
-(44, 15, 0, '2016-03-07 22:49:02'),
-(45, 18, 0, '2016-03-07 22:49:02'),
-(46, 23, 0, '2016-03-07 22:49:02'),
-(47, 26, 0, '2016-03-07 22:49:02'),
-(48, 31, 0, '2016-03-07 22:49:02'),
-(49, 34, 0, '2016-03-07 22:49:02'),
-(50, 39, 0, '2016-03-07 22:49:02'),
-(51, 4, 0, '2016-03-07 22:49:37'),
-(52, 8, 0, '2016-03-07 22:49:37'),
-(53, 1, 0, '2016-03-07 22:49:57'),
-(54, 7, 0, '2016-03-07 22:49:57'),
-(55, 2, 0, '2016-03-07 22:50:27'),
-(56, 8, 0, '2016-03-07 22:50:27'),
-(57, 9, 0, '2016-03-07 22:50:28'),
-(58, 14, 0, '2016-03-07 22:50:28'),
-(59, 17, 0, '2016-03-07 22:50:28'),
-(60, 23, 0, '2016-03-07 22:50:28'),
-(61, 27, 0, '2016-03-07 22:50:28'),
-(62, 32, 0, '2016-03-07 22:50:28'),
-(63, 36, 0, '2016-03-07 22:50:28'),
-(64, 38, 0, '2016-03-07 22:50:28'),
-(65, 42, 0, '2016-03-08 22:52:50'),
-(66, 45, 0, '2016-03-08 22:52:50'),
-(67, 42, 0, '2016-03-08 22:56:18'),
-(68, 45, 0, '2016-03-08 22:56:18'),
-(69, 42, 0, '2016-03-08 22:57:06'),
-(70, 45, 0, '2016-03-08 22:57:06'),
-(71, 42, 0, '2016-03-08 22:57:35'),
-(72, 45, 0, '2016-03-08 22:57:35'),
-(73, 42, 0, '2016-03-08 22:57:45'),
-(74, 45, 0, '2016-03-08 22:57:46'),
-(75, 42, 0, '2016-03-08 22:58:15'),
-(76, 45, 0, '2016-03-08 22:58:15'),
-(77, 42, 0, '2016-03-08 22:58:17'),
-(78, 45, 0, '2016-03-08 22:58:17'),
-(79, 41, 0, '2016-03-08 22:58:21'),
-(80, 45, 0, '2016-03-08 22:58:21'),
-(81, 41, 0, '2016-03-08 22:58:25'),
-(82, 45, 0, '2016-03-08 22:58:25'),
-(83, 44, 0, '2016-03-08 22:58:37'),
-(84, 46, 0, '2016-03-08 22:58:37'),
-(90, 1, 0, '2016-03-09 20:55:51'),
-(91, 7, 0, '2016-03-09 20:55:51'),
-(92, 11, 0, '2016-03-09 20:55:51'),
-(93, 14, 0, '2016-03-09 20:55:51'),
-(94, 19, 0, '2016-03-09 20:55:51'),
-(95, 21, 0, '2016-03-09 20:55:51'),
-(96, 25, 0, '2016-03-09 20:55:51'),
-(97, 29, 0, '2016-03-09 20:55:51'),
-(98, 33, 0, '2016-03-09 20:55:51'),
-(99, 38, 0, '2016-03-09 20:55:51'),
-(100, 42, 0, '2016-03-09 20:56:14'),
-(101, 45, 0, '2016-03-09 20:56:14'),
-(102, 82, 0, '2016-03-09 20:58:24'),
-(103, 81, 0, '2016-03-09 21:00:23'),
-(104, 81, 0, '2016-03-09 21:04:28'),
-(105, 82, 0, '2016-03-09 21:04:38'),
-(106, 82, 0, '2016-03-09 21:04:58'),
-(107, 82, 0, '2016-03-09 21:05:23'),
-(108, 86, 0, '2016-03-09 21:07:49'),
-(109, 87, 0, '2016-03-09 21:08:11'),
-(110, 87, 0, '2016-03-09 21:13:47'),
-(111, 43, 0, '2016-03-09 21:14:07'),
-(112, 46, 0, '2016-03-09 21:14:07'),
-(113, 88, 0, '2016-03-09 21:14:21');
 
 -- --------------------------------------------------------
 
@@ -528,7 +305,7 @@ ALTER TABLE `usuarios_niveles`
 -- AUTO_INCREMENT de la tabla `campaigns`
 --
 ALTER TABLE `campaigns`
-  MODIFY `campaigns_k` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `campaigns_k` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT de la tabla `datos_administradores`
 --
@@ -553,22 +330,22 @@ ALTER TABLE `datos_encuestados`
 -- AUTO_INCREMENT de la tabla `encuestas`
 --
 ALTER TABLE `encuestas`
-  MODIFY `encuestas_k` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `encuestas_k` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT de la tabla `encuestas_preguntas`
 --
 ALTER TABLE `encuestas_preguntas`
-  MODIFY `encuestas_preguntas_k` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `encuestas_preguntas_k` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 --
 -- AUTO_INCREMENT de la tabla `encuestas_preguntas_opciones`
 --
 ALTER TABLE `encuestas_preguntas_opciones`
-  MODIFY `encuestas_preguntas_opciones_k` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
+  MODIFY `encuestas_preguntas_opciones_k` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT de la tabla `encuestas_resultados`
 --
 ALTER TABLE `encuestas_resultados`
-  MODIFY `encuestas_resultados_k` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=114;
+  MODIFY `encuestas_resultados_k` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
@@ -588,3 +365,7 @@ ALTER TABLE `usuarios_niveles`
 --
 ALTER TABLE `encuestas_preguntas`
   ADD CONSTRAINT `restriccion` FOREIGN KEY (`encuestas_k`) REFERENCES `encuestas` (`encuestas_k`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
